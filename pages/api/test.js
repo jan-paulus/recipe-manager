99936ae54fedfdbd8fc5698e63b3dee96670689a
Jan Paulus <jan.paulus@mercedes-benz.io>
2023-06-23 07:30:05 +0200

    use env vars to configure directories

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,10 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs";
+import path from "path";
 
 export default function test(req, res) {
-  if (fs.existsSync(`recipes`)) {
-    fs.writeFileSync(`recipes/${+new Date()}.md`, "cool");
+  const recipeDirPath = path.join(
+    process.env.HOST_VOLUME_PATH,
+    process.env.RECIPE_DIRECTORY_PATH
+  );
+
+  if (fs.existsSync(recipeDirPath)) {
+    fs.writeFileSync(path.join(recipeDirPath, `${+new Date()}.md`), "cool");
     res.status(200).json({ name: "Success" });
+    return;
   }
+
   res.status(200).json({ name: "Failed" });
 }
--- pages/api/verify.js
+++ pages/api/verify.js
@@ -1,7 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs";
+import path from "path";
 
 export default function hello(req, res) {
-  const result = fs.readdirSync("recipes/");
+  const recipeDirPath = path.join(
+    process.env.HOST_VOLUME_PATH,
+    process.env.RECIPE_DIRECTORY_PATH
+  );
+
+  const result = fs.readdirSync(recipeDirPath);
   res.status(200).json(JSON.stringify(result));
 }
